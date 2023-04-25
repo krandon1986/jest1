8cb9727748ff732186212af3ed7b115a2499c363
krandon1986 <krandon1986@yahoo.co.uk>
2023-04-25 13:55:52 +0000

    Add three more addition test in the game test file and three function in the main game file

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -18,4 +18,13 @@ describe("game object contains correct keys", () => {
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
       });
+      test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+      });
+      test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+      });
+      test("choices contrain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+      });
 });
